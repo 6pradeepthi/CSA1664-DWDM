60441d46010f73f557bdcead8b327570fa81ac31
6pradeepthi <122279279+6pradeepthi@users.noreply.github.com>
2024-08-02 12:58:52 +0530

    Add files via upload

---
+++ barplot.R
@@ -0,0 +1,4 @@
+x=c("jan","feb","mar","apr")
+y=c(10,30,50,70)
+colors="blue"
+barplot(names.arg=x,y,col=colors)
---
+++ boxplot.R
@@ -0,0 +1,3 @@
+points_scored=c(35,36,37,38,39,40)
+boxplot(points_scored,main="Box-Plot points scored by tennis player",
+xlab="players",ylab="points scored",col="blue",border="black")
